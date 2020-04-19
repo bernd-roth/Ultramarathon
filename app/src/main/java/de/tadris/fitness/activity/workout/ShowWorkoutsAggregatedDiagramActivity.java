d99e44f3f775f522d1ca7b9d7dd0e593169703b5
thrillfall <thrillfall@disroot.org>
2020-04-19 23:55:56 +0200

    show date of workout with maximum speed or distance

--- app/src/main/java/de/tadris/fitness/activity/workout/ShowWorkoutsAggregatedDiagramActivity.java
+++ app/src/main/java/de/tadris/fitness/activity/workout/ShowWorkoutsAggregatedDiagramActivity.java
@@ -40,6 +40,8 @@ public class ShowWorkoutsAggregatedDiagramActivity extends FitoTrackActivity {
     String selectedWorkoutType = WorkoutType.RUNNING.id;
     double fastestAverage;
     int greatestDistance;
+    private String fastestAverageDate;
+    private String greatestDistanceDate;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -69,17 +71,29 @@ public class ShowWorkoutsAggregatedDiagramActivity extends FitoTrackActivity {
     private void setMaxValues() {
         TextView fastestAverageTextView = findViewById(R.id.fastestAverage);
         fastestAverageTextView.setText(Math.floor(fastestAverage) + " " + distanceUnitUtils.getDistanceUnitSystem().getSpeedUnit());
+        TextView fastestAverageDateTextView = findViewById(R.id.fastestAverageDate);
+        fastestAverageDateTextView.setText(fastestAverageDate);
         TextView greatestDistanceTextView = findViewById(R.id.greatestDistance);
         greatestDistanceTextView.setText(distanceUnitUtils.getDistance(greatestDistance));
+        TextView greatestDistanceDateTextView = findViewById(R.id.greatestDistanceDate);
+        greatestDistanceDateTextView.setText(greatestDistanceDate);
     }
 
     private ArrayList<Entry> calculateValues(Workout[] workouts) {
         fastestAverage = greatestDistance = 0;
+        fastestAverageDate = greatestDistanceDate = "--";
         ArrayList<Entry> averageSpeedValues = new ArrayList<>();
-        for (Workout workout: workouts) {
+        for (Workout workout : workouts) {
             double averageSpeed = distanceUnitUtils.getDistanceUnitSystem().getSpeedFromMeterPerSecond((workout.getAvgSpeedTotal()));
-            fastestAverage = Math.max(fastestAverage, averageSpeed);
-            greatestDistance = Math.max(greatestDistance, workout.length);
+            if (fastestAverage < averageSpeed) {
+                fastestAverage = averageSpeed;
+                fastestAverageDate = workout.getDateString();
+            }
+
+            if (greatestDistance < workout.length) {
+                greatestDistance = workout.length;
+                greatestDistanceDate = workout.getDateString();
+            }
             averageSpeedValues.add(new Entry((float) workout.end, (float) averageSpeed));
         }
 
@@ -91,13 +105,14 @@ public class ShowWorkoutsAggregatedDiagramActivity extends FitoTrackActivity {
         ArrayList<String> workoutTypes = createChoicesList();
 
         ArrayAdapter<String> SpinnerAdapter = new ArrayAdapter<String>
-                (this, android.R.layout.simple_spinner_item, workoutTypes){
+                (this, android.R.layout.simple_spinner_item, workoutTypes) {
             public View getView(int position, View convertView,
                                 ViewGroup parent) {
                 View v = super.getView(position, convertView, parent);
                 ((TextView) v).setTextColor(Color.parseColor("#E30D81"));
                 return v;
             }
+
             public View getDropDownView(int position, View convertView,
                                         ViewGroup parent) {
                 View v = super.getDropDownView(position, convertView,
@@ -145,7 +160,7 @@ public class ShowWorkoutsAggregatedDiagramActivity extends FitoTrackActivity {
     }
 
     private LineChart createChart() {
-        LineChart chart  = findViewById(R.id.aggregatedWorkoutsAverageSpeed);
+        LineChart chart = findViewById(R.id.aggregatedWorkoutsAverageSpeed);
 
         AxisBase xAxis = chart.getXAxis();
         xAxis.setValueFormatter(new ValueFormatter() {
@@ -163,10 +178,12 @@ public class ShowWorkoutsAggregatedDiagramActivity extends FitoTrackActivity {
         chart.getAxisLeft().setTextColor(Color.DKGRAY);
         chart.getAxisLeft().setTextSize(15f);
         chart.getAxisRight().setTextColor(Color.DKGRAY);
-        chart.getAxisRight().setTextSize(15f);;
+        chart.getAxisRight().setTextSize(15f);
+        ;
 
         chart.getXAxis().setTextColor(Color.DKGRAY);
-        chart.getXAxis().setTextSize(15f);;
+        chart.getXAxis().setTextSize(15f);
+        ;
 
         chart.setNoDataText(getString(R.string.no_workouts_recorded_for_this_activity));
         chart.setNoDataTextColor(Color.DKGRAY);
