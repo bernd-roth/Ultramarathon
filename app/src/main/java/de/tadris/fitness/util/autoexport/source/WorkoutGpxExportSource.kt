2968a47b90cba0ed2c9432118ad408174d9655c3
jannis <jannis@tadris.de>
2022-05-12 16:51:27 +0200

    Auto export HTTP request: fixed duplicate header entry "FitoTrack-Type"

--- app/src/main/java/de/tadris/fitness/util/autoexport/source/WorkoutGpxExportSource.kt
+++ app/src/main/java/de/tadris/fitness/util/autoexport/source/WorkoutGpxExportSource.kt
@@ -48,7 +48,7 @@ class WorkoutGpxExportSource(private val workoutId: Long) : ExportSource {
             File(file), mapOf(
                 "FitoTrack-Type" to ExportSource.EXPORT_SOURCE_WORKOUT_GPX,
                 "FitoTrack-Timestamp" to workout.start.toString(),
-                "FitoTrack-Type" to workout.workoutTypeId,
+                "FitoTrack-Workout-Type" to workout.workoutTypeId,
                 "FitoTrack-Comment" to workout.safeComment,
             )
         )
