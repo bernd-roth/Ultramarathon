add77d1f1edbb253dc2c1c145dc56e7442f415b1
jannis <jannis@tadris.de>
2022-11-16 14:59:28 +0100

    Fix crashes on Android <= 8

--- app/src/main/java/de/tadris/fitness/ui/statistics/TextToggle.java
+++ app/src/main/java/de/tadris/fitness/ui/statistics/TextToggle.java
@@ -1,8 +1,29 @@
+/*
+ * Copyright (c) 2022 Jannis Scheibe <jannis@tadris.de>
+ *
+ * This file is part of FitoTrack
+ *
+ * FitoTrack is free software: you can redistribute it and/or modify
+ *     it under the terms of the GNU General Public License as published by
+ *     the Free Software Foundation, either version 3 of the License, or
+ *     (at your option) any later version.
+ *
+ *     FitoTrack is distributed in the hope that it will be useful,
+ *     but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *     GNU General Public License for more details.
+ *
+ *     You should have received a copy of the GNU General Public License
+ *     along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 package de.tadris.fitness.ui.statistics;
 
 import android.content.Context;
 import android.content.res.TypedArray;
 import android.util.AttributeSet;
+import android.view.View;
+import android.view.animation.AccelerateDecelerateInterpolator;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
@@ -17,7 +38,7 @@ public class TextToggle extends LinearLayout {
 
     public TextView currentTitle;
     public TextView swapTitle;
-    private TextView toggleArrows;
+    private View toggleArrows;
     private boolean swapped;
 
     private IOnToggleListener onToggleListener;
@@ -53,7 +74,13 @@ public class TextToggle extends LinearLayout {
         currentTitle.setText(swapTitle.getText());
         swapTitle.setText(current);
 
-        toggleArrows.setText(toggleArrows.getText() == "\u296E" ? "\u296F" : "\u296E");
+        toggleArrows.clearAnimation();
+        toggleArrows.setRotation(0f);
+        toggleArrows.animate()
+                .rotationBy(180f)
+                .setDuration(200)
+                .setInterpolator(new AccelerateDecelerateInterpolator())
+                .start();
 
         swapped = !swapped;
 
